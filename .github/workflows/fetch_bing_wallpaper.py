82ea6a2e2c0c39563a2a76affc7c0cd83768f4a7
linmew <lm1194@hanweb.com>
2023-04-25 12:01:12 +0800

    update yaml

--- .github/workflows/fetch_bing_wallpaper.py
+++ .github/workflows/fetch_bing_wallpaper.py
@@ -49,7 +49,8 @@ def update_readme(wallpapers):
     if not new_wallpapers:
         return
 
-    for image_url, image_date, image_caption in new_wallpapers[::-1]:
+    for image_url, image_date, image_caption, image_title in new_wallpapers[::-1]:
+        save_image(image_url, image_date, image_title)
         header = table.th
         if not header:
             header = soup.new_tag("th", colspan="3")
