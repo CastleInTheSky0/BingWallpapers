1a6c62b10736191c35783d4e22cae576e9de1f88
linmew <lm1194@hanweb.com>
2023-04-26 18:33:46 +0800

    update py

--- .github/workflows/fetch_bing_wallpaper.py
+++ .github/workflows/fetch_bing_wallpaper.py
@@ -7,17 +7,21 @@ URL = "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=8&mkt=en-US"
 BASE_URL = "https://www.bing.com"
 
 def fetch_bing_wallpapers():
-    response = requests.get(URL)
-    data = response.json()
+    # 使用127.0.0.1:10809代理请求数据
+    proxies = {
+        "http": "http://127.0.0.1:10809",
+        "https": "http://127.0.0.1:10809"
+    }
 
+    response = requests.get(URL, proxies=proxies)
+    data = response.json()
     wallpapers = []
     for image in data["images"]:
         image_url = BASE_URL + image["url"]
         image_date = image["enddate"]
-        image_startdate = image["startdate"]
         image_caption = image["copyright"]
         image_title = image["title"]
-        wallpapers.append((image_url, image_date, image_startdate, image_caption, image_title))
+        wallpapers.append((image_url, image_date, image_caption, image_title))
 
     return wallpapers
 
@@ -30,6 +34,7 @@ def save_image(image_url, image_date, image_title):
     image_path = f"old_wallpapers/{year_month}/{image_name}.jpg"
 
     if os.path.exists(image_path):
+        print("壁纸已存在！")
         return
 
     response = requests.get(image_url)
@@ -50,27 +55,41 @@ def update_readme(wallpapers):
         soup.append(table)
 
     existing_images = [img["src"] for img in table.find_all("img")]
+    # 获取table中所有的img标签本身和p标签本身
+    old_wallpapers = []
+    if table.th:
+        image_url = table.th.img["src"]
+        image_title = table.th.img["alt"]
+        p_content = table.th.p.string.split(" - ")
+        image_date = p_content[0].strip()
+        image_caption = p_content[1].strip()
+        old_wallpapers.append((image_url, image_date, image_caption, image_title))
+    for row in table.find_all("tr")[1:]:
+        for cell in row.find_all("td"):
+            if cell.img:
+                image_url = cell.img["src"]
+                image_title = cell.img["alt"]
+                p_content = cell.p.string.split(" - ")
+                image_date = p_content[0].strip()
+                image_caption = p_content[1].strip()
+                old_wallpapers.append((image_url, image_date, image_caption, image_title))
 
     new_wallpapers = sorted([wp for wp in wallpapers if wp[0] not in existing_images], key=lambda x: x[1], reverse=True)
 
     if not new_wallpapers:
+        print('没有新的壁纸！')
         return
 
-    latest_image_url, latest_image_date, _, latest_image_caption, latest_image_title = new_wallpapers[0]
+    latest_image_url, latest_image_date, latest_image_caption, latest_image_title = new_wallpapers[0]
 
     header = table.th
 
     if header:
-        # 去除可能存在的换行和空格
         current_header_date = datetime.strptime(header.p.string.split(" - ")[0].strip(), "%Y%m%d")
         latest_image_datetime = datetime.strptime(latest_image_date, "%Y%m%d")
 
         if latest_image_datetime <= current_header_date:
             return
-
-        # 保存原表头壁纸信息
-        old_header_image_url = header.img["src"]
-        old_header_caption = header.p.string
 
         header.img.decompose()
         header.p.decompose()
@@ -81,51 +100,36 @@ def update_readme(wallpapers):
         table.insert(0, first_row)
 
     save_image(latest_image_url, latest_image_date, latest_image_title)
-    new_image = soup.new_tag("img", src=latest_image_url, width="100%")
+    new_image = soup.new_tag("img", src=latest_image_url, alt=latest_image_title, width="100%")
     header.append(new_image)
     header_caption = soup.new_tag("p")
     header_caption.string = f"{latest_image_date} - {latest_image_caption}"
     header.append(header_caption)
 
-    # 将原表头壁纸插入新壁纸列表的第一个位置
-    old_header_date = current_header_date.strftime("%Y%m%d")
-    new_wallpapers.insert(1, (old_header_image_url, old_header_date, None, old_header_caption, None))
+    seen = set()
+    all_wallpapers = []
 
-    cells = table.find_all("td")
+    for wp in old_wallpapers + new_wallpapers:
+        if wp[0] not in seen:
+            all_wallpapers.append(wp)
+            seen.add(wp[0])
 
-    for i, (image_url, image_date, _, image_caption, image_title) in enumerate(new_wallpapers[1:]):
-        if i >= len(cells):
-            break
-
-        cell = cells[i]
-        cell.clear()
-        save_image(image_url, image_date, image_title)
-        new_image = soup.new_tag("img", src=image_url, width="100%")
-        cell.append(new_image)
-        cell_caption = soup.new_tag("p")
-        cell_caption.string = f"{image_date} - {image_caption}"
-        cell.append(cell_caption)
-
-    # 计算所需的行数
-    num_rows = (len(new_wallpapers) - 1) // 3
-    if (len(new_wallpapers) - 1) % 3 != 0:
-        num_rows += 1
-
-    # 保留最多 10 行（30 个单元格）
-    num_rows = min(num_rows, 10)
-
-    # 添加或删除行以匹配所需的行数
-    rows = table.find_all("tr")[1:]
-    while len(rows) < num_rows:
-        new_row = soup.new_tag("tr")
-        for _ in range(3):
-            new_cell = soup.new_tag("td")
-            new_row.append(new_cell)
-        table.append(new_row)
-        rows.append(new_row)
-    while len(rows) > num_rows:
-        rows[-1].decompose()
-        rows.pop()
+    all_wallpapers.sort(key=lambda x: x[1], reverse=True)
+    
+    for i, row in enumerate(table.find_all("tr")[1:]):
+        for j, cell in enumerate(row.find_all("td")):
+            index = i * 3 + j + 1
+            if index < len(all_wallpapers):
+                image_url, image_date, image_caption, image_title = all_wallpapers[index]
+                cell.clear()
+                save_image(image_url, image_date, image_title)
+                new_image = soup.new_tag("img", src=image_url, alt=image_title, height="180px")
+                cell.append(new_image)
+                cell_caption = soup.new_tag("p")
+                cell_caption.string = f"{image_date} - {image_caption}"
+                cell.append(cell_caption)
+            else:
+                cell.clear()
 
     with open("README.md", "w") as f:
         f.write(str(soup))
